d687dc8173309ff8ff4e9637ab93d2663b4cd5e9
wuwentao <1037936099@qq.com>
2022-08-11 16:40:35 +0800

    feat: Promise.all

--- diy-lib/utils/promise.js
+++ diy-lib/utils/promise.js
@@ -7,23 +7,26 @@ Promise.diyAll = function(promises) {
     return new Promise((resolve, reject) => {
         if (!promises || typeof promises[Symbol.iterator] !== 'function') throw new Error('Type Error')
 
-        let count = 0
-        let len = 0
-        const res = []
+        let count = 0 // 完成计数
+        let len = 0 // 迭代器个数
+        const res = [] // 返回数组
 
+        // 遍历迭代器
         for (const item of promises) {
-            const index = len
-            len++
+            const index = len++ // 返回值索引
             Promise.resolve(item).then(data => {
-                res[index] = data
+                res[index] = data // 保存返回值
                 if (++count === len) {
+                    // 全部resolve
                     resolve(res)
                 }
             }).catch(e => {
+                // 有失败
                 reject(e)
             })
         }
 
+        // 处理空Iterator的情况
         if (len === 0) {
             resolve([])
         }
